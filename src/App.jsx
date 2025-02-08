fceb3f8f9aa1421487f175df911d1db03c75a66e
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-08 21:03:52 +0500

    Main Entry Point

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
 
 const App = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const showModalHandler = () => {
+    setModalIsVisible(true);
+  };
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 };
 
