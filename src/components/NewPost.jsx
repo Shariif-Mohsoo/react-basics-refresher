c79e4df99bf4c91d1e86c971cc7389fdf6401008
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-09 08:15:36 +0500

    component to create post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react";
+
 import classes from "./NewPost.module.css";
 
-const NewPost = () => {
+const NewPost = (props) => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const bodyChangeHandler = (event) => {
+    // console.log(event.target.value);
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    // console.log(event.target.value);
+    setEnteredAuthor(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    props.onAddPost(postData);
+    setEnteredAuthor("");
+    setEnteredBody("");
+    props.onCancel();
+    // console.log(postData);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea
+          value={enteredBody}
+          onChange={bodyChangeHandler}
+          id="body"
+          required
+          rows={3}
+        />
       </p>
       <p>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" required />
+        <input
+          value={enteredAuthor}
+          onChange={authorChangeHandler}
+          type="text"
+          id="name"
+          required
+        />
+      </p>
+      <p className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button>Submit</button>
       </p>
     </form>
   );
