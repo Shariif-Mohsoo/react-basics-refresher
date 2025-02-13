f27b3e6a6532d399a940799417fb7e810263ba71
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-13 07:08:28 +0500

    Modal Component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
 import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Modal = ({ children, onClose }) => {
+const Modal = ({ children }) => {
+  const navigate = useNavigate();
+  function closeHandler() {
+    // to navigate/move to the parent route.
+    navigate("..");
+  }
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={closeHandler} />
       <dialog open className={classes.modal}>
         {children}
       </dialog>
